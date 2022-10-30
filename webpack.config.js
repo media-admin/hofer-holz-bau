f91fb8d2b12c6f1df3f753cf185a050a3dc8c4a6
media-admin <markus.tritremmel@media-lab.at>
2022-10-30 19:09:23 +0000

    Working on default setup for new projects.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin') // For creating a minimized version of style.css
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const fse = require('fs-extra')
 
@@ -14,10 +14,14 @@ const postCSSPlugins = [
 	require('autoprefixer')
 ]
 
+
 class RunAfterCompile {
 	apply(compiler) {
 		compiler.hooks.done.tap('Copy images', function() {
 			fse.copySync('./app/assets/images', './dist/assets/images')
+		})
+		compiler.hooks.done.tap('Copy vendor files', function() {
+			fse.copySync('./app/vendor', './dist/assets/vendor')
 		})
 	}
 }
@@ -41,7 +45,23 @@ let config = {
 	plugins: pages,
 	module: {
 		rules: [
-			cssConfig
+			cssConfig,
+			{
+				 test: /\.(jpg|png|svg|gif)$/,
+				 type: 'asset/resource',
+				 generator: {
+						//publicPath: '../fonts/',
+						filename: 'assets/images/[name][ext][query]'
+					}
+			 },
+			{
+				test: /\.(svg|eot|woff|woff2|ttf)$/,
+				 type: 'asset/resource',
+				 generator: {
+					 //publicPath: '../fonts/',
+					 filename: 'assets/fonts/[name][ext][query]'
+				 }
+			},
 		]
 	}
 }
@@ -58,7 +78,7 @@ if (currentTask == 'dev') {
 		hot: true,
 		port: 3000,
 		host: '0.0.0.0',
-		https: true,
+		https: false,
 	}
 	config.mode = 'development'
 }
@@ -66,7 +86,9 @@ if (currentTask == 'dev') {
 if (currentTask == 'build') {
 	config.module.rules.push({
 		test: /\.js$/,
-		exclude: /(node_modules)/,
+		exclude: [
+			/(node_modules)/,
+		],
 		use: {
 			loader: 'babel-loader',
 			options: {
@@ -74,7 +96,9 @@ if (currentTask == 'build') {
 			}
 		}
 	})
+
 	cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+
 	config.output = {
 		filename: '[name].[chunkhash].js',
 		chunkFilename: '[name].[chunkhash].js',
@@ -82,15 +106,26 @@ if (currentTask == 'build') {
 	}
 	config.mode = 'production'
 	config.optimization = {
-		splitChunks: {chunks:'all', minSize: 1000},
-		minimize: true,
-		minimizer: [`...`, new CssMinimizerPlugin()]
+		runtimeChunk: 'single',
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all'
+				}
+			},
+
+		},
+		// minimize: true,
+		// minimizer: [`...`, new CssMinimizerPlugin()],
 	}
 	config.plugins.push(
 		new CleanWebpackPlugin(),
 		new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
 		new RunAfterCompile()
 	)
+
 }
 
 module.exports = config
