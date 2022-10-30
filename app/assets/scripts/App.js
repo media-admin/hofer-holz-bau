8452c7292ed40f81bf77e3ef040f4546603d75a1
media-admin <markus.tritremmel@media-lab.at>
2022-10-30 19:21:32 +0000

    Further working on default project template

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,40 @@
-import '../styles/styles.css'
+import 'lightbox2/dist/css/lightbox.css';
+import "../styles/styles.css"
+
+import 'lightbox2'
+import "jquery"
+import "lazysizes"
+import "slick-slider"
+
+import StickyHeader from "./modules/StickyHeader"
+import BackToTop from "./modules/BackToTop"
+import Dywc from "./modules/Dywc"
+import CoockieNotice from "./modules/CoockieNotice"
+
+let stickyHeader = new StickyHeader()
+let backToTop = new BackToTop()
+let dywc = new Dywc()
+let cookieNotice = new CockieNotice()
+
+let modal
+
+
+document.querySelectorAll(".open-modal").forEach((el) => {
+	el.addEventListener("click", (e) => {
+		e.preventDefault()
+		if (typeof modal == "undefined") {
+			import(/* webpackChunkName: "modal" */ "./modules/Modal")
+				.then((x) => {
+					modal = new x.default()
+					setTimeout(() => modal.openTheModal(), 20)
+				})
+				.catch(() => console.log("Ein Problem ist aufgetreten."))
+		} else {
+			modal.openTheModal()
+		}
+	})
+})
+
 
 if (module.hot) {
 	module.hot.accept()
